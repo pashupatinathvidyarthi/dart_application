b730c943f9cdb7398d876832ecd22213a99ea04e
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-03 18:00:52 +0530

    Dart

--- Dart Programming Language/1.2_Var_Dyn_Types.dart
+++ Dart Programming Language/1.2_Var_Dyn_Types.dart
@@ -3,4 +3,17 @@ void main()
   int age=30;
   bool isMarried=true;
   print(age==20); //Returns false
+
+
+  var name='Dart'; //var is short for variable and we do not say the type of variable  
+  
+  //var name=20; //Through an error i.e we caanot use same variable name again
+
+  print(name.runtimeType); //To check type of variable
+
+
+  dynamic phone="+917563809967";
+  print(phone.runtimeType);
+
+  phone=true; 
 }
