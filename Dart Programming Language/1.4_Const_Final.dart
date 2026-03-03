41fae5be7a8a962679b408d55995c5de8f6d9093
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-03 18:47:11 +0530

    Dart

---
+++ Dart Programming Language/1.4_Const_Final.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  final double pi=3.14159; //Here final is used to fixed the value of variable i.e we can not change the value of pi after this line.
+  //pi=3455.677 //Through an error
+
+  
+}
