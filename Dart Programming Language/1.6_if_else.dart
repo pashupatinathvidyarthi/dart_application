a53ce97dbded0065d88ba333598d2b099c2a9d08
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-03 19:08:14 +0530

    Dart

---
+++ Dart Programming Language/1.6_if_else.dart
@@ -0,0 +1,30 @@
+void main()
+{
+  //temp<10 -> very coold
+  // temp between 10 and 18 -> cold
+  // temp between 19 and 24 -> normal
+  // tem between 25 and 30 -> hot
+  // temp > 30 -> very hot
+  final temp=29.4;
+
+  if(temp<10)
+  {
+    print("Very Cold");
+  }
+ else if (temp>=10 && temp <=18)
+ {
+  print("Cold");
+ }
+ else if(temp>=19 && temp<=18)
+ {
+  print("Normal");
+ }
+ else if(temp>=25 && temp<=30)
+ {
+  print("Hot");
+ }
+ else
+ {
+  print("Very Hot");
+ }
+}
