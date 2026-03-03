1bc528a60468524e3b095bfe7c644cf3aaba1a56
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-03 17:44:51 +0530

    Dart

--- Dart Programming Language/1.1_Number_Types.dart
+++ Dart Programming Language/1.1_Number_Types.dart
@@ -2,7 +2,9 @@ void main()
 {
   int age=30;
   double pi=3.14159;
+  num zipcode= 8022.12; //num datatype can store both integer and deciaml number both 
   print(age);
   print(age.isEven);
   print(pi);
+  print(zipcode);
 }
