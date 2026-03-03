d6799b5204f1feb4be65cab2f8203472960ea29f
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-03 17:49:05 +0530

    Dart

--- Dart Programming Language/1.2_String.dart
+++ Dart Programming Language/1.2_String.dart
@@ -1,8 +1,10 @@
 void main()
 {
   String name="Pashupati";
-  String framework='F';
+  String framework='Flutter';
   print(name);
   print(framework);
   print(name.length);
+  print("I Love $framework");
+  print("I Love Flutter");
 }
